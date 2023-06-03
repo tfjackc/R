1caedae05ee4a748ce9d8f5995bd8b943f54fa3f
tfjackc <colpitt.jack@gmail.com>
2023-06-03 00:26:38 -0700

    progress on R spatial

--- geoapp/app.R
+++ geoapp/app.R
@@ -1,10 +1,18 @@
+library(tidyverse)
 library(maps)
 library(leaflet)
 library(shiny)
 library(here)
-library(tidyverse)
+library(sp)
+library(rgdal)
+
 
 eq <- read_csv(here("2.5_month.csv"))
+#url <- "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+#earthquakes <- readOGR(url)
+#eqsf <- st_as_sf(earthquakes)
+#ggplot() +
+#   geom_sf(data = eqsf) # plots geom point of spatial dataframe
 
 leaflet(eq) %>%
   addTiles() %>%
@@ -14,7 +22,7 @@ ui <- fluidPage(
   titlePanel("USGS Earthquakes"),
   leafletOutput("mymap"),
   fluidRow(column(2,
-                  sliderInput("slider", "Select the magnitude", 0, 9, 2),
+                  sliderInput("slider", "Select the magnitude", 2, 9, 2),
                   
                   radioButtons("radio", h3("Select the location source"),
                                choices = list("ak" = "ak", "ci" = "ci", "hv" = "hv", "ld" = "ld", "mb" = "mb", "nc" = "nc", "nm" = "nm", "nn" = "nn", "pr" = "pr", "pt" = "pt", "se" = "se", "us" = "us", "uu" = "uu", "uw" = "uw"), selected = "nc")
